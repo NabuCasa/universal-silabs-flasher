acbbcbc78e66a5537def9b2fc877a4619220d959
puddly <32534428+puddly@users.noreply.github.com>
2022-11-16 13:29:11 -0500

    Use gpiod instead of gpiozero

--- universal_silabs_flasher/flasher.py
+++ universal_silabs_flasher/flasher.py
@@ -1,15 +1,12 @@
 from __future__ import annotations
 
-import os
 import enum
 import time
 import typing
 import asyncio
 import logging
-import os.path
-import contextlib
 
-import gpiozero
+import gpiod
 import bellows.ezsp
 import async_timeout
 import bellows.types
@@ -27,30 +24,31 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def _send_gpio_pattern(pin_states: dict[int, list[bool]], toggle_delay: float) -> None:
-    os.environ.setdefault("GPIOZERO_PIN_FACTORY", "native")
+    chip = gpiod.chip(0, gpiod.chip.OPEN_BY_NUMBER)
+    lines = {pin: chip.get_line(pin) for pin in pin_states.keys()}
 
-    with contextlib.ExitStack() as stack:
-        devices = {}
+    config = gpiod.line_request()
+    config.consumer = "universal-silabs-flasher"
+    config.request_type = gpiod.line_request.DIRECTION_OUTPUT
 
+    try:
         # Open the pins and set their initial states
-        for pin in pin_states.keys():
+        for pin, line in lines.items():
             state = pin_states[pin][0]
-            device = gpiozero.OutputDevice(pin=pin, initial_value=state)
-
-            stack.enter_context(device)
-            devices[pin] = device
+            line.request(config, int(state))
 
         time.sleep(toggle_delay)
 
         # Send all subsequent states
         for i in range(1, len(pin_states[pin])):
-            for pin, states in pin_states.items():
-                if states[i]:
-                    devices[pin].on()
-                else:
-                    devices[pin].off()
+            for pin, line in lines.items():
+                line.set_value(int(pin_states[pin][i]))
 
             time.sleep(toggle_delay)
+    finally:
+        # Clean up and ensure the GPIO pins are reset to inputs
+        for line in lines.values():
+            line.set_direction_input()
 
 
 async def send_gpio_pattern(
