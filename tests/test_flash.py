df6cd2d4dab3db929a2730cd0e9be06d52612a66
puddly <32534428+puddly@users.noreply.github.com>
2022-11-28 10:44:57 -0500

    Ensure the "does not exist" error message is shown for missing devices

---
+++ tests/test_flash.py
@@ -0,0 +1,20 @@
+import click
+import pytest
+
+from universal_silabs_flasher.flash import SerialPort
+
+
+def test_click_serialport_validation():
+    assert SerialPort().convert("/dev/null", None, None) == "/dev/null"
+    assert SerialPort().convert("COM123", None, None) == "COM123"
+    assert SerialPort().convert("socket://1.2.3.4", None, None) == "socket://1.2.3.4"
+
+    with pytest.raises(click.BadParameter) as exc_info:
+        assert SerialPort().convert("http://1.2.3.4", None, None)
+
+    assert "invalid URL scheme" in exc_info.value.message
+
+    with pytest.raises(click.BadParameter) as exc_info:
+        assert SerialPort().convert("/dev/serial/by-id/does-not-exist", None, None)
+
+    assert "does not exist" in exc_info.value.message
