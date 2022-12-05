b61f7e81552856d72fd42631dce2da14e70076c6
puddly <32534428+puddly@users.noreply.github.com>
2022-12-05 16:41:05 -0500

    0.0.8 version bump

--- universal_silabs_flasher/__init__.py
+++ universal_silabs_flasher/__init__.py
@@ -1,6 +1,6 @@
 MAJOR_VERSION = 0
 MINOR_VERSION = 0
-PATCH_VERSION = 7
+PATCH_VERSION = 8
 
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
