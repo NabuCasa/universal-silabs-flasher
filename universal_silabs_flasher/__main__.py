73685a26a94d4b669f8e617082fdffa6decf556b
puddly <32534428+puddly@users.noreply.github.com>
2026-02-20 20:46:53 -0500

    Fix main, for real

--- universal_silabs_flasher/__main__.py
+++ universal_silabs_flasher/__main__.py
@@ -1,6 +1,12 @@
 import asyncio
 
-from .flash import main
+from .flash import main as async_main
+
+
+def main():
+    """Main entry point."""
+    asyncio.run(async_main())
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
